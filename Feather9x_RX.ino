dce7c810e9575d88d853ff8f97559ccf49069bca
Vishal Mishra <51082676+vishu-fcb@users.noreply.github.com>
2019-07-29 14:32:54 +0200

    Source code uploaded

---
+++ Feather9x_RX.ino
@@ -0,0 +1,112 @@
+
+#include <SPI.h>
+#include <RH_RF95.h>
+
+
+#define RFM95_CS 8
+#define RFM95_RST 4
+#define RFM95_INT 3
+
+#if defined(ESP8266)
+  /* for ESP w/featherwing */ 
+  #define RFM95_CS  2    // "E"
+  #define RFM95_RST 16   // "D"
+  #define RFM95_INT 15   // "B"
+
+#elif defined(ESP32)  
+  /* ESP32 feather w/wing */
+  #define RFM95_RST     27   // "A"
+  #define RFM95_CS      33   // "B"
+  #define RFM95_INT     12   //  next to A
+
+#elif defined(NRF52)  
+  /* nRF52832 feather w/wing */
+  #define RFM95_RST     7   // "A"
+  #define RFM95_CS      11   // "B"
+  #define RFM95_INT     31   // "C"
+  
+#elif defined(TEENSYDUINO)
+  /* Teensy 3.x w/wing */
+  #define RFM95_RST     9   // "A"
+  #define RFM95_CS      10   // "B"
+  #define RFM95_INT     4    // "C"
+#endif
+
+
+// Change to 434.0 or other frequency, must match RX's freq!
+#define RF95_FREQ 868.1
+
+// Singleton instance of the radio driver
+RH_RF95 rf95(RFM95_CS, RFM95_INT);
+
+// Blinky on receipt
+#define LED 13
+
+void setup()
+{
+  pinMode(LED, OUTPUT);
+  pinMode(RFM95_RST, OUTPUT);
+  digitalWrite(RFM95_RST, HIGH);
+
+  Serial.begin(115200);
+  while (!Serial) {
+    delay(1);
+  }
+  delay(100);
+
+  Serial.println("Feather LoRa RX Test!");
+
+  // manual reset
+  digitalWrite(RFM95_RST, LOW);
+  delay(10);
+  digitalWrite(RFM95_RST, HIGH);
+  delay(10);
+
+  while (!rf95.init()) {
+    Serial.println("LoRa radio init failed");
+    while (1);
+  }
+  Serial.println("LoRa radio init OK!");
+
+  // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM
+  if (!rf95.setFrequency(RF95_FREQ)) {
+    Serial.println("setFrequency failed");
+    while (1);
+  }
+  Serial.print("Set Freq to: "); Serial.println(RF95_FREQ);
+
+  rf95.setTxPower(23, false);
+}
+
+void loop()
+{
+  if (rf95.available())
+  {
+    // Should be a message for us now
+    uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
+    uint8_t len = sizeof(buf);
+    int b7=0,b6=0,b5=0,b4=0,b3=0,b2=0,b1=0,b0=0,bit_val=0;
+
+    if (rf95.recv(buf, &len))
+    {
+      digitalWrite(LED, HIGH);
+      RH_RF95::printBuffer("Received: ", buf, len);
+      Serial.print("Got: ");
+      int val = atoi((char*)buf);
+      //Serial.println(val);
+      Serial.println(char(val));                                            
+//
+//      char data[] = "";
+//      char c = 't';
+//      strncat(data, &c, 1);
+//      Serial.println(data);
+
+      Serial.print("RSSI: ");
+      Serial.println(rf95.lastRssi(), DEC);
+    }
+    else
+    {
+      Serial.println("Receive failed");
+    }
+  }
+}
---
+++ transmitter.ino
@@ -0,0 +1,120 @@
+#include <SPI.h>
+#include <RH_RF95.h>
+
+//for feather m0
+#define RFM95_CS 8
+#define RFM95_RST 4
+#define RFM95_INT 3
+
+
+// Change to 434.0 or other frequency, must match RX's freq!
+#define RF95_FREQ 868.1
+#define LED 13
+uint32_t timer = millis();
+RH_RF95 rf95(RFM95_CS, RFM95_INT);
+char bufRX485[RH_RF95_MAX_MESSAGE_LEN];
+void setup()
+{
+  pinMode(RFM95_RST, OUTPUT);
+  digitalWrite(RFM95_RST, HIGH);
+  pinMode(LED, OUTPUT);
+  digitalWrite(LED, HIGH);
+
+  while (!Serial1);
+  Serial.begin(19200);
+  delay(100);
+  Serial1.begin(19200);
+  delay(100);
+
+  Serial.println("Feather LoRa TX Test!");
+  delay(10);
+
+  // manual reset
+  digitalWrite(RFM95_RST, LOW);
+  delay(10);
+  digitalWrite(RFM95_RST, HIGH);
+  delay(10);
+
+  while (!rf95.init()) {
+    Serial.println("LoRa radio init failed");
+    while (1);
+  }
+  Serial.println("LoRa radio init OK!");
+  if (!rf95.setFrequency(RF95_FREQ)) {
+    while (1);
+  }
+  Serial.print("Set Freq to: "); Serial.println(RF95_FREQ);
+
+  rf95.setTxPower(23, false);
+  delay(1000);
+  digitalWrite(LED, LOW);
+}
+
+int16_t packetnum = 0;  // packet counter, we increment per xmission
+
+void loop()
+{
+  char br;
+  uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
+  uint8_t len = sizeof(buf);
+  if (Serial1.available()) {
+    br = Serial1.read();
+
+    if (getTelegram() > 0) {
+      sendLora();
+    }
+  }
+}
+
+int getTelegram() {
+  char *p = bufRX485, str, br, output[9]="";
+  int i , n , rs485count = 0;
+  for (i = 0; i <= 1000; i++) {
+    if (Serial1.available()) {
+      br = Serial1.read();
+//      if(isalpha(br)){
+//      Serial.println("dwdsdd");        
+//      }
+//      else {
+//      Serial.println("int");        
+//      }
+      
+      Serial.println(br);
+      *p = br;
+      p++;
+      rs485count++;
+      itoa(br, output, 10);
+      Serial.println(output);
+
+      puts(output);
+      ++br;
+      rf95.send((uint8_t*)output, sizeof(output));
+      rf95.waitPacketSent();
+
+    }
+    delay(10);
+  }
+  *p = 0;
+  return (-1);
+}
+void sendLora() {
+//  int i = 0, n ;
+//  char bufTX[RH_RF95_MAX_MESSAGE_LEN];
+//  uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
+//  uint8_t len = sizeof(buf);
+//  uint8_t len2 = sizeof(bufRX485);
+//  char *p = bufTX;
+//
+//
+//  for (i = 0; i <= len2; i++) {
+//    Serial.println(bufRX485[i], HEX);
+//    if ((bufRX485[i-1] == 0x10) and (bufRX485[i]==0x03))
+//    break;
+//  }
+//  Serial.println(bufRX485);
+//  Serial.println(i);
+//  //rf95.send((uint8_t *)bufRX485, strlen(bufRX485) + 1);
+//  delay(1);
+//  rf95.waitPacketSent();
+//  delay(10);
+}
